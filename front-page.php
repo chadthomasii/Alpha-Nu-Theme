c04186df1d46feaec5614c4f9a6a44bcdfc65a84
chadthomasii <chad@Skylar-2.local>
2019-03-14 22:41:17 -0400

    On news featured page added dynamic link to the page with the template 'All News.'

--- front-page.php
+++ front-page.php
@@ -1,76 +1,122 @@
-<?php get_header(); ?>
+<?php /* Template Name: Featured News Page */  get_header(); ?>
 
-<?php get_template_part('template-parts/template','heading-news'); ?>
 
-<div class="all-news">
+<?php 
 
-    <div class="all-articles">
+    $query = new WP_Query( array( 'post_type' => 'post',
+                                  'posts_per_page' => '1',) ); // Post loop settings ?> 
 
-        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-            <a href="<?php echo get_permalink() ?>">
-                <div class="all-articles-single">
-                    <div class="all-article-thumbnail">
-                        <div class="thumbnail-image">
-                            <img src="<?php the_post_thumbnail_url() ?>"/>
-                        </div>
-                    </div>
+<?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
 
-                    <div class="all-article-content">
-                        <div class="all-news-title">
-                            <h1><?php the_title() ?></h1>
-                        </div>
-
-                        <div class="all-news-content">
-                            <p><?php the_excerpt(20)?></p>
-                        </div>
-                        
-                        <?php 
-            
-                            $category = get_the_category();
-                            $categories = '';
-                        
-
-                            //Get the categories of category element, loop through them
-                            for($i = 0; $i < count($category); $i++)
-                            {
-                                if($i < 2)
-                                {
-                                        //Does not add a "|" to the last element in the array
-                                    if($i != key(end($category)) || count($category) <= 1)
-                                    {
-                                        $categories .= $category[$i]->name .= " "; 
-                                    }
-
-                                    else
-                                    {
-                                        $categories .= $category[$i]->name . ' | ';
-                                    }
-                                }
-                                
-
-                            }
-
-                            
-                            // var_dump($category);
-                        ?>
-                        <div class="all-news-date">
-                            <p style="font-weight: bold;"><?php the_time('m.j.y')?> // <?php echo $categories; ?></p>
-                        </div>
-                    </div>
-                </div>
-            </a>
-        <?php endwhile; endif; ?>
-        
-
-    </div>
+<a href="<?php echo get_permalink()?>">
+    <div class="news-featured-image" style="background:linear-gradient(rgba(0,0,0,0.4),rgba(0,0,0,0.4)), url('<?php the_post_thumbnail_url()?>'); background-size: cover; background-position: 50% 50%; background-repeat: no-repeat;">
     
-
-
-    <div class="all-news-sidebar">
-        <h1>Sidebar</h1>
+        <div>
+            <h1><?php the_title(); ?></h1>
+        </div>
+        
     </div>
+</a>
 
+<?php endwhile; endif;  wp_reset_postdata();?>
 
+<div class="featured-articles">
+
+    
+    <?php 
+
+        $cat = get_category_by_slug("featured");
+
+    
+        $query = new WP_Query( array( 'post_type' => 'post',
+                                        'posts_per_page' => '3',
+                                        'category__not_in' => $cat->term_id) ); // Post loop settings ?> 
+    
+   
+    <?php  if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); //Start the post loop?> 
+    <a href="<?php echo get_permalink()?>">
+        <div class="featured-article">
+
+            <div class="featured-article-image" style="background:url('<?php the_post_thumbnail_url()?>'); background-size: cover; background-position: 50% 50%; background-repeat: no-repeat;"></div>
+
+            <div class="featured-article-body">
+                <h3><?php the_title(); ?></h3>
+                <?php the_excerpt(20); ?>
+            </div>
+
+            <?php 
+            
+                $category = get_the_category();
+                $categories = '';
+              
+
+                //Get the categories of category element, loop through them
+                for($i = 0; $i < count($category); $i++)
+                {
+                    if($i < 2)
+                    {
+                            //Does not add a "|" to the last element in the array
+                        if($i != key(end($category)) || count($category) <= 1)
+                        {
+                            $categories .= $category[$i]->name .= " "; 
+                        }
+
+                        else
+                        {
+                            $categories .= $category[$i]->name . ' | ';
+                        }
+                    }
+                    
+
+                }
+
+                
+                // var_dump($category);
+            ?>
+            
+            <div class="featured-article-date">
+                <p><?php the_time('M')?> // <?php echo $categories; ?> </p>
+            </div>
+
+            
+        </div>
+    </a>
+
+    <?php endwhile; ?>
+
+    <?php else : ?>
+
+        <h1 style="text-align: center;">Please Add Posts to the page.</h1>
+
+    <?php endif; wp_reset_postdata(); // Clost loop, if, post data settings ?> 
+    
+   
 </div>
+
+<?php
+    $args = array(
+        'post_type' => 'page',
+        'meta_query' => array(
+            array(
+                'key' => '_wp_page_template',
+                'value' => 'all-news.php'
+            )
+        )
+    );
+
+    $the_pages = new WP_Query( $args );
+    
+?>
+
+<?php  if ( $the_pages->have_posts() ) : while ( $the_pages->have_posts() ) : $the_pages->the_post(); //Start the post loop?> 
+
+<div class="more-news">
+    <a href="<?php echo get_permalink()?>">More News -></a>
+</div>
+
+<?php endwhile; endif;  wp_reset_postdata();?>
+
+
+
 
 <?php get_footer(); ?>
---
+++ page-all-news.php
@@ -0,0 +1,78 @@
+<?php /* Template Name: All News Page */  get_header(); ?>
+
+<?php get_template_part('template-parts/template','heading-news'); ?>
+
+<div class="all-news">
+
+    <div class="all-articles">
+
+    <?php $query = new WP_Query( array( 'post_type' => 'post',) ); // Post loop settings ?> 
+
+    <?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>            
+<a href="<?php echo get_permalink() ?>">
+                <div class="all-articles-single">
+                    <div class="all-article-thumbnail">
+                        <div class="thumbnail-image">
+                            <img src="<?php the_post_thumbnail_url() ?>"/>
+                        </div>
+                    </div>
+
+                    <div class="all-article-content">
+                        <div class="all-news-title">
+                            <h1><?php the_title() ?></h1>
+                        </div>
+
+                        <div class="all-news-content">
+                            <p><?php the_excerpt(20)?></p>
+                        </div>
+                        
+                        <?php 
+            
+                            $category = get_the_category();
+                            $categories = '';
+                        
+
+                            //Get the categories of category element, loop through them
+                            for($i = 0; $i < count($category); $i++)
+                            {
+                                if($i < 2)
+                                {
+                                        //Does not add a "|" to the last element in the array
+                                    if($i != key(end($category)) || count($category) <= 1)
+                                    {
+                                        $categories .= $category[$i]->name .= " "; 
+                                    }
+
+                                    else
+                                    {
+                                        $categories .= $category[$i]->name . ' | ';
+                                    }
+                                }
+                                
+
+                            }
+
+                            
+                            // var_dump($category);
+                        ?>
+                        <div class="all-news-date">
+                            <p style="font-weight: bold;"><?php the_time('m.j.y')?> // <?php echo $categories; ?></p>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        <?php endwhile; endif; ?>
+        
+
+    </div>
+    
+
+
+    <div class="all-news-sidebar">
+        <h1>Sidebar</h1>
+    </div>
+
+
+</div>
+
+<?php get_footer(); ?>
